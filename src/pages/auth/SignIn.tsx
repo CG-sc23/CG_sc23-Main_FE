de88a1394263d1e55bd2ab8713142eb12ea86049
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-11-07 22:51:40 +0900

    LayoutContainer 적용

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,35 +1,29 @@
-import Link from 'next/link';
-import { FormEvent, useState } from 'react';
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
-import { motion } from 'framer-motion';
+import Link from "next/link";
+import { FormEvent, useState } from "react";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import { motion } from "framer-motion";
 
-import useSignIn from '@/hooks/auth/useSignIn';
-import { Schema, validatedOnChange } from '@/libs/utils/validate';
+import useSignIn from "@/hooks/auth/useSignIn";
+import { Schema, validatedOnChange } from "@/libs/utils/validate";
 
-import Card from '@/components/Card';
-import { colors } from '@/components/constant/color';
-import InputWithLabel from '@/components/InputWithLabel';
+import LayoutContainer from "@/components/Auth/LayoutContainer";
+import { colors } from "@/components/constant/color";
+import InputWithLabel from "@/components/InputWithLabel";
 
 const Form = styled.form`
+  width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 2rem;
+  gap: 1rem;
 `;
 
-const Button = styled.button<{ bgColor?: string }>`
-  background: none;
-  outline: none;
-  border: none;
-
+const Button = styled.button<{ bgColor?: string; color?: string }>`
   padding: 1rem 0;
-
-  background-color: ${(props) =>
-    props.bgColor ? props.bgColor : colors.yellow300};
-  color: ${(props) => (props.color ? props.color : colors.white)};
-
-  border-radius: 5px;
-  font-size: larger;
+  background-color: ${(props) => props.bgColor};
+  color: ${(props) => props.color ?? "white"};
+  border-radius: 1rem;
+  font-size: 1rem;
   font-weight: 600;
   cursor: pointer;
 `;
@@ -37,7 +31,7 @@ const Button = styled.button<{ bgColor?: string }>`
 const errorVariants = {
   initial: {
     opacity: 0.2,
-    y: '-10px',
+    y: "-10px",
   },
   animate: {
     opacity: 1,
@@ -46,146 +40,89 @@ const errorVariants = {
 };
 
 export default function SignIn() {
-  const [email, setEmail] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const { signIn, kakao, naver, error, isPending } = useSignIn();
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('로그인');
+    console.log("로그인");
     signIn({ email, password });
   };
 
   return (
-    <div
-      css={css`
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      `}
-    >
-      <Card>
-        <div
-          css={css`
-            width: 5rem;
-            height: 5rem;
-            background-color: aqua;
-            margin: auto;
-            border-radius: 50%;
-          `}
+    <LayoutContainer>
+      <Form onSubmit={onSubmit}>
+        <InputWithLabel
+          type="email"
+          label="Email"
+          value={email}
+          error={emailError}
+          onChange={validatedOnChange({
+            schema: Schema.email,
+            setValue: setEmail,
+            setError: setEmailError,
+          })}
         />
-        <Form onSubmit={onSubmit}>
-          <span
-            css={css`
-              padding: 0 2rem;
-              font-size: 1.5rem;
-
-              word-break: break-all;
-              line-break: auto;
-              text-align: center;
-            `}
-          >
-            By logging in, you accept our <Link href="/">terms</Link> and
-            privacy <Link href="/">policy</Link>.
-          </span>
-          <InputWithLabel
-            type="email"
-            label="Email"
-            value={email}
-            error={emailError}
-            onChange={validatedOnChange({
-              schema: Schema.email,
-              setValue: setEmail,
-              setError: setEmailError,
-            })}
-          />
-          <InputWithLabel
-            type="password"
-            label="Password"
-            value={password}
-            error={passwordError}
-            autoFocus={false}
-            onChange={validatedOnChange({
-              schema: Schema.password,
-              setValue: setPassword,
-              setError: setPasswordError,
-            })}
-          />
-          <Button type="submit" bgColor={colors.black}>
-            {isPending ? '작업 중' : '로그인'}
-          </Button>
-          <motion.div
-            variants={errorVariants}
-            initial="initial"
-            animate={error !== '' ? 'animate' : 'initial'}
-            exit="initial"
-            css={css`
-              padding: 0.2rem 0;
-              height: 1.5rem;
-              font-size: 1rem;
-              color: ${colors.red400};
-            `}
-          >
-            {error}
-          </motion.div>
-        </Form>
-        <div
+        <InputWithLabel
+          type="password"
+          label="Password"
+          value={password}
+          error={passwordError}
+          autoFocus={false}
+          onChange={validatedOnChange({
+            schema: Schema.password,
+            setValue: setPassword,
+            setError: setPasswordError,
+          })}
+        />
+        <Button type="submit" bgColor={colors.black}>
+          {isPending ? "작업 중" : "로그인"}
+        </Button>
+        <motion.div
+          variants={errorVariants}
+          initial="initial"
+          animate={error !== "" ? "animate" : "initial"}
+          exit="initial"
           css={css`
-            display: flex;
-            gap: 16px;
-            color: ${colors.grey300};
+            padding: 0.2rem 0;
+            height: 1.5rem;
+            font-size: 1rem;
+            color: ${colors.red400};
           `}
         >
-          <hr
-            css={css`
-              border: none;
-              outline: none;
-
-              background-color: ${colors.grey300};
-              height: 1px;
-              flex: 1;
-            `}
-          />
-          OR
-          <hr
-            css={css`
-              border: none;
-              outline: none;
-
-              background-color: ${colors.grey300};
-              height: 1px;
-              flex: 1;
-            `}
-          />
-        </div>
-        <div
-          css={css`
-            position: relative;
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-          `}
-        >
-          <Button onClick={kakao}>Login with KAKAO</Button>
-          <Button onClick={naver} bgColor={colors.green300}>
-            Login with Naver
-          </Button>
-        </div>
-        <div
-          css={css`
-            display: flex;
-            justify-content: space-between;
-          `}
-        >
-          <Link href="/auth/SignUp?step=email">Sign Up</Link>
-          <Link href="/auth/PasswordReset">Find Password</Link>
-        </div>
-      </Card>
-    </div>
+          {error}
+        </motion.div>
+      </Form>
+      <div
+        css={css`
+          width: 100%;
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          gap: 1rem;
+        `}
+      >
+        <Button onClick={kakao} bgColor={colors.kakao} color={"black"}>
+          Login with KAKAO
+        </Button>
+        <Button onClick={naver} bgColor={colors.naver} color={"white"}>
+          Login with Naver
+        </Button>
+      </div>
+      <div
+        css={css`
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+        `}
+      >
+        <Link href="/auth/SignUp?step=email">Sign Up</Link>
+        <Link href="/auth/PasswordReset">Find Password</Link>
+      </div>
+    </LayoutContainer>
   );
 }
