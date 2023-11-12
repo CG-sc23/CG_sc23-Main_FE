a9c6bc7702ee33acf86adb99633f14ccff0bbb9d
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-11-12 16:22:44 +0900

    signup 부분 css 적용

--- src/components/Auth/StepSignUp.tsx
+++ src/components/Auth/StepSignUp.tsx
@@ -1,22 +1,25 @@
-import { FormEvent, type Dispatch, type SetStateAction, useState } from 'react';
-import { motion } from 'framer-motion';
-import { css } from '@emotion/react';
+import { FormEvent, type Dispatch, type SetStateAction, useState } from "react";
+import { motion } from "framer-motion";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 
-import type { Status } from '@/hooks/auth/useSignUpFunnel';
-import { toUpperCaseFirstLetter } from '@/libs/utils';
+import type { Status } from "@/hooks/auth/useSignUpFunnel";
+import { toUpperCaseFirstLetter } from "@/libs/utils";
 import {
   Schema,
   type SchemaKey,
   validatedOnChange,
-} from '@/libs/utils/validate';
+} from "@/libs/utils/validate";
 
-import client from '@/api/client';
-import InputWithLabel from '../InputWithLabel';
-import { colors } from '../constant/color';
-import Dropzone from '../Dropzone';
+import client from "@/api/client";
+import InputWithLabel from "../InputWithLabel";
+import { colors } from "../constant/color";
+import Dropzone from "../Dropzone";
 
 type Props = {
   step: string;
+  stepCount: number;
+  title: string;
   status: Status;
   current: string | File;
   setCurrent: Dispatch<SetStateAction<string | File>>;
@@ -26,8 +29,29 @@ type Props = {
   isDisabled: boolean;
 };
 
+const Header = styled.h1`
+  margin-bottom: 3rem;
+  font-weight: 500;
+  font-size: 1.5rem;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+`;
+
+const DropZoneWrapper = styled.div`
+  width: 18rem;
+  height: 16rem;
+`;
+
 export default function StepSignUp({
   step,
+  stepCount,
+  title,
   status,
   current,
   setCurrent,
@@ -36,7 +60,7 @@ export default function StepSignUp({
   setError,
   isDisabled,
 }: Props) {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
   const [sentCode, setSentCode] = useState(false);
   const [codeVerified, setCodeVerified] = useState(false);
 
@@ -60,13 +84,14 @@ export default function StepSignUp({
 
     if (!sentCode) {
       const emailInput = form.querySelector(
-        'input[name="email"]',
+        'input[name="email"]'
       ) as HTMLInputElement | null;
       if (!emailInput) return;
 
       const emailValue = emailInput.value;
 
       setIsLoading(true);
+
       const result = await client
         .signUpEmailVerify({ email: emailValue })
         // eslint-disable-next-line no-console
@@ -75,19 +100,21 @@ export default function StepSignUp({
 
       if (!result) return;
       if (!result?.ok) return setError(result?.reason as string);
+
       setSentCode(true);
+
       return;
     }
 
     if (!codeVerified) {
       const emailInput = form.querySelector(
-        'input[name="email"]',
+        'input[name="email"]'
       ) as HTMLInputElement | null;
       if (!emailInput) return;
       const emailValue = emailInput.value;
 
       const codeInput = form.querySelector(
-        'input[name="code"]',
+        'input[name="code"]'
       ) as HTMLInputElement | null;
       if (!codeInput) return;
 
@@ -103,27 +130,30 @@ export default function StepSignUp({
         .catch(console.error)
         .finally(() => setIsLoading(false));
       if (!result) return;
-      if (!result?.ok) return setError(result?.reason as string);
+      if (!result?.ok) return setError("유효하지 않은 토큰입니다.");
       setCodeVerified(true);
+      setError("");
       return;
     }
 
     nextStep(current);
   }
 
+  // 이메일 입력 시 버튼 내 텍스트 변경
   function getButtonText() {
     if (isLoading) {
-      return '작업 중';
+      return `잠시만 기다려 주세요...`;
     }
     if (!sentCode) {
-      return '인증번호 전송';
+      return `인증번호 전송 (${stepCount}/6)`;
     }
     if (!codeVerified) {
-      return '인증번호 확인';
+      return `인증번호 확인 (${stepCount}/6)`;
     }
-    return '다음';
+    return `다음 (${stepCount}/6)`;
   }
 
+  // 이메일 입력 상태 전이
   function getEmailDisabled() {
     if (!sentCode) {
       return isDisabled;
@@ -135,51 +165,28 @@ export default function StepSignUp({
     return false;
   }
 
-  if (status === 'loading') return <div>loading</div>;
-  if (status === 'fulfilled') return <div>done</div>;
-  if (status === 'rejected') return <div>fail</div>;
+  if (status === "loading") return <div>loading</div>;
+  if (status === "fulfilled") return <div>done</div>;
+  if (status === "rejected") return <div>fail</div>;
 
-  if (step === 'profile_image') {
-    return (
-      <form key={step} onSubmit={onSubmit}>
-        <Dropzone
-          onFileAdded={(file) => setCurrent(file)}
-          isDisabled={isDisabled}
-        />
-        <button
-          css={css`
-            background: none;
-            outline: none;
-            border: none;
-
-            width: 100%;
-            padding: 0.8rem 0;
-            border-radius: 10px;
-
-            font-size: 1.2rem;
-            color: ${colors.white};
-            background-color: ${isDisabled ? colors.blue200 : colors.blue400};
-            cursor: ${isDisabled ? 'not-allowed' : 'pointer'};
-
-            transition: background-color 0.2s ease;
-
-            :hover {
-              background-color: ${isDisabled ? colors.blue100 : colors.blue300};
-            }
-          `}
-          type="submit"
-          disabled={isDisabled}
-        >
-          다음
-        </button>
-      </form>
-    );
-  }
-
+  // step 정보
   const stepWithText = step as SchemaKey;
-  if (stepWithText === 'email') {
+
+  // 1. email 입력 단계
+  if (stepWithText === "email") {
     return (
-      <motion.form onSubmit={onSubmitEmail}>
+      <motion.form
+        onSubmit={onSubmitEmail}
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 1rem;
+          width: 100%;
+        `}
+      >
+        <Header>{title}</Header>
+        {/* email 입력 */}
         <InputWithLabel
           label={toUpperCaseFirstLetter(step)}
           type={step}
@@ -190,43 +197,57 @@ export default function StepSignUp({
             setValue: setCurrent as Dispatch<SetStateAction<string>>,
             setError,
           })}
-          error={error}
+          error=""
           readonly={sentCode}
         />
+        {/* code 입력 */}
         {sentCode ? (
-          <InputWithLabel
-            label="인증번호"
-            type="text"
-            name="code"
-            value={code as string}
-            onChange={(e) => setCode(e.target.value)}
-            readonly={codeVerified}
-            error={error}
-          />
+          <div
+            css={css`
+              position: relative;
+              width: 100%;
+            `}
+          >
+            <InputWithLabel
+              label="인증번호"
+              type="text"
+              name="code"
+              value={code as string}
+              onChange={(e) => setCode(e.target.value)}
+              readonly={codeVerified}
+              error={error}
+            />
+            {codeVerified ? (
+              <span
+                css={css`
+                  position: absolute;
+                  top: 3rem;
+                  font-size: 0.8rem;
+                  font-weight: 600;
+                  color: ${colors.green700};
+                `}
+              >
+                인증 완료!
+              </span>
+            ) : null}
+          </div>
         ) : null}
         <motion.button
           css={css`
-            background: none;
-            outline: none;
-            border: none;
-
             width: 100%;
             padding: 0.8rem 0;
             border-radius: 10px;
-
             font-size: 1.2rem;
             color: ${colors.white};
             background-color: ${getEmailDisabled()
-              ? colors.blue200
-              : colors.blue400};
-            cursor: ${getEmailDisabled() ? 'not-allowed' : 'pointer'};
-
+              ? colors.grey500
+              : colors.black};
+            cursor: ${getEmailDisabled() ? "not-allowed" : "pointer"};
             transition: background-color 0.2s ease;
-
             :hover {
               background-color: ${getEmailDisabled()
-                ? colors.blue100
-                : colors.blue300};
+                ? colors.grey600
+                : colors.black};
             }
           `}
           type="submit"
@@ -238,9 +259,51 @@ export default function StepSignUp({
     );
   }
 
+  // 프로필 이미지 입력 단계
+  if (step === "profile_image") {
+    return (
+      <Form
+        key={step}
+        onSubmit={onSubmit}
+        css={css`
+          gap: 2rem;
+        `}
+      >
+        <Header>{title}</Header>
+        <DropZoneWrapper>
+          <Dropzone
+            onFileAdded={(file) => setCurrent(file)}
+            isDisabled={isDisabled}
+          />
+        </DropZoneWrapper>
+        <button
+          css={css`
+            width: 100%;
+            padding: 0.8rem 0;
+            border-radius: 10px;
+            font-size: 1.2rem;
+            color: ${colors.white};
+            background-color: ${isDisabled ? colors.grey500 : colors.black};
+            cursor: ${isDisabled ? "not-allowed" : "pointer"};
+            transition: background-color 0.2s ease;
+            :hover {
+              background-color: ${isDisabled ? colors.grey600 : colors.black};
+            }
+          `}
+          type="submit"
+          disabled={isDisabled}
+        >
+          다음 ({stepCount}/6)
+        </button>
+      </Form>
+    );
+  }
+
+  // 그 외의 단계들
   if (Schema[stepWithText]) {
     return (
-      <form key={step} onSubmit={onSubmit}>
+      <Form key={step} onSubmit={onSubmit}>
+        <Header>{title}</Header>
         <InputWithLabel
           label={toUpperCaseFirstLetter(step)}
           type={step}
@@ -254,36 +317,30 @@ export default function StepSignUp({
         />
         <button
           css={css`
-            background: none;
-            outline: none;
-            border: none;
-
             width: 100%;
             padding: 0.8rem 0;
             border-radius: 10px;
-
             font-size: 1.2rem;
             color: ${colors.white};
-            background-color: ${isDisabled ? colors.blue200 : colors.blue400};
-            cursor: ${isDisabled ? 'not-allowed' : 'pointer'};
-
+            background-color: ${isDisabled ? colors.grey500 : colors.black};
+            cursor: ${isDisabled ? "not-allowed" : "pointer"};
             transition: background-color 0.2s ease;
-
             :hover {
-              background-color: ${isDisabled ? colors.blue100 : colors.blue300};
+              background-color: ${isDisabled ? colors.grey600 : colors.black};
             }
           `}
           type="submit"
           disabled={isDisabled}
         >
-          다음
+          다음 ({stepCount}/6)
         </button>
-      </form>
+      </Form>
     );
   }
 
   return (
-    <form key={step} onSubmit={onSubmit}>
+    <Form key={step} onSubmit={onSubmit}>
+      <Header>{title}</Header>
       <InputWithLabel
         label={toUpperCaseFirstLetter(step)}
         type={step}
@@ -293,30 +350,24 @@ export default function StepSignUp({
       />
       <button
         css={css`
-          background: none;
-          outline: none;
-          border: none;
-
           width: 100%;
           padding: 0.8rem 0;
           border-radius: 10px;
 
           font-size: 1.2rem;
           color: ${colors.white};
-          background-color: ${isDisabled ? colors.blue200 : colors.blue400};
-          cursor: ${isDisabled ? 'not-allowed' : 'pointer'};
-
+          background-color: ${isDisabled ? colors.grey500 : colors.black};
+          cursor: ${isDisabled ? "not-allowed" : "pointer"};
           transition: background-color 0.2s ease;
-
           :hover {
-            background-color: ${isDisabled ? colors.blue100 : colors.blue300};
+            background-color: ${isDisabled ? colors.grey600 : colors.black};
           }
         `}
         type="submit"
         disabled={isDisabled}
       >
-        다음
+        다음 ({stepCount}/6)
       </button>
-    </form>
+    </Form>
   );
 }
