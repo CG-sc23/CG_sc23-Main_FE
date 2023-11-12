26e39090efd7dcb1d72091feec341ae96a2c8b54
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-11-12 16:23:47 +0900

    회원가입 단계별 제목 + 단계 번호 보여주기 위한 코드 수정

--- src/components/Auth/FunnelSignUp.tsx
+++ src/components/Auth/FunnelSignUp.tsx
@@ -1,36 +1,43 @@
-import { useState } from 'react';
-import useSignUpFunnel from '@/hooks/auth/useSignUpFunnel';
-import StepSignUp from './StepSignUp';
+import { useState } from "react";
+import useSignUpFunnel from "@/hooks/auth/useSignUpFunnel";
+import StepSignUp from "./StepSignUp";
 
 const list = [
-  'email',
-  'password',
-  'name',
-  'profile_image',
-  'github_link',
-  'short_description',
+  { id: "email", title: "이메일을 입력해주세요." },
+  { id: "password", title: "비밀번호를 입력해주세요." },
+  { id: "name", title: "닉네임일 입력해주세요." },
+  { id: "profile_image", title: "프로필 이미지를 등록해주세요." },
+  { id: "github_link", title: "깃허브 주소를 입력해주세요." },
+  { id: "short_description", title: "간단한 자기소개를 부탁드려요." },
 ] as const;
 
-const optionalList = ['profile_image', 'github_link', 'short_description'];
+const optionalList = ["profile_image", "github_link", "short_description"];
 
 export default function FunnelSignUp() {
-  const [current, setCurrent] = useState<string | File>('');
-  const [error, setError] = useState('');
+  const [current, setCurrent] = useState<string | File>("");
+  const [error, setError] = useState("");
 
-  const { Funnel, steps, nextStep, status } = useSignUpFunnel(list, {
-    afterStepChange: setCurrent,
-  });
+  const { Funnel, steps, nextStep, status } = useSignUpFunnel(
+    list.map((el) => el.id),
+    {
+      afterStepChange: setCurrent,
+    }
+  );
 
   const isDisabled =
-    (current === '' || error !== '') && !optionalList.includes(status);
+    (current === "" || error !== "") && !optionalList.includes(status);
+
+  const getTitle = (id: string) => list.filter((el) => el.id === id)[0]?.title;
 
   return (
     <Funnel>
-      {steps.map((val) => {
+      {steps.map((val, idx) => {
         return (
           <Funnel.Step name={val} key={val}>
             <StepSignUp
               step={val}
+              stepCount={idx + 1}
+              title={getTitle(val)}
               status={status}
               current={current}
               setCurrent={setCurrent}
