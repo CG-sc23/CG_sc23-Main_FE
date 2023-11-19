52e8c92781452c86485a9c9c676009140aa9ccd8
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-11-20 01:49:52 +0900

    [ETC] Task 글 정보 mock 데이터

--- src/libs/constant/test.ts
+++ src/libs/constant/test.ts
@@ -47,48 +47,132 @@ export const UserDatas = [
 // Test Data for Project in Feed
 export const ProjectDatas = [
   {
-    title :"나는 프론트엔드가 싫어요",
-    projectId :1234,
-    projectTitle :"인증/인가",
-    owner:"임준혁",
-    likes:12,
-    id:12,
-    created_at : '2023.11.19'
+    projectId: 1234,
+    projectTitle: "프레임워크 없는 프론트엔드 톺아보기",
+    likes: 12,
+    thumbnail: "/project.jpg",
+    status: "in_progress",
+    short_description:
+      "배고픔을 이기기 위한 자들의 프로젝트. 겨울이 오고 있습니다. 취업 시장은 그 어느때보다 힘듭니다. 이럴때 일 수록 더 힘을 내서 프로젝트를 더해야 합니다. 부족함을 간절함으로 채워봅시다. 이를 위해 우리는 프로젝트원을 구합니다. 같이할 열정 넘치는 분들을 구합니다. 꼭 취뽀합시다.",
   },
   {
-    title :"나는 프론트엔드가 싫어요",
-    projectId :1234,
-    projectTitle :"인증/인가",
-    owner:"임준혁",
-    likes:12,
-    id:12,
-    created_at : '2023.11.19'
+    projectId: 1234,
+    projectTitle: "배고픈자들을위한프로젝트이자춘식이를위한프로젝트",
+    likes: 12,
+    thumbnail: "/project.jpg",
+    status: "in_progress",
+    short_description:
+      "배고픔을 이기기 위한 자들의 프로젝트. 겨울이 오고 있습니다. ",
   },
   {
-    title :"나는 프론트엔드가 싫어요",
-    projectId :1234,
-    projectTitle :"인증/인가",
-    owner:"임준혁",
-    likes:12,
-    id:12,
-    created_at : '2023.11.19'
+    projectId: 1234,
+    projectTitle: "스벨트를 잘써보자",
+    likes: 12,
+    thumbnail: "/project.jpg",
+    status: "completed",
+    short_description:
+      "배고픔을 이기기 위한 자들의 프로젝트. 겨울이 오고 있습니다. 취업 시장은 그 어느때보다 힘듭니다. 이럴때 일 수록 더 힘을 내서 프로젝트를 더해야 합니다. ",
   },
   {
-    title :"나는 프론트엔드가 싫어요",
-    projectId :1234,
-    projectTitle :"인증/인가",
-    owner:"임준혁",
-    likes:12,
-    id:12,
-    created_at : '2023.11.19'
+    projectId: 1234,
+    projectTitle: "로그인/로그아웃 기능구현",
+    likes: 12,
+    thumbnail: "/project.jpg",
+    status: "terminated",
+    short_description:
+      "배고픔을 이기기 위한 자들의 프로젝트. 겨울이 오고 있습니다. 취업 시장은 그 어느때보다 힘듭니다. 이럴때 일 수록 더 힘을 내서 프로젝트를 더해야 합니다. 부족함을 간절함으로 채워봅시다. 이를 위해 우리는 프로젝트원을 구합니다. ",
   },
   {
-    title :"나는 프론트엔드가 싫어요",
-    projectId :1234,
-    projectTitle :"인증/인가",
-    owner:"임준혁",
-    likes:12,
-    id:12,
-    created_at : '2023.11.19'
-  }
-]
+    projectId: 1234,
+    projectTitle: "Access Token, OAuth",
+    likes: 12,
+    thumbnail: "/project.jpg",
+    status: "in_progress",
+    short_description:
+      "배고픔을 이기기 위한 자들의 프로젝트. 겨울이 오고 있습니다. 취업 시장은 그 어느때보다 힘듭니다. 이럴때 일 수록 더 힘을 내서 프로젝트를 더해야 합니다. 부족함을 간절함으로 채워봅시다. 이를 위해 우리는 프로젝트원을 구합니다. 같이할 열정 넘치는 분들을 구합니다. ",
+  },
+];
+
+// Test Data for Task in Feed
+export const TaskDatas = [
+  {
+    title: "프로젝트에 인증/인가를 도입하며 배운점",
+    content: `# 프로젝트를 통해 배운 점 
+## 배고픔을 이겨내는 방법 
+## 배고픔은 항상 다가온다`,
+    author: "임준혁",
+    id: 123,
+    projectTitle: "리액트 톺아보기",
+    projectId: 1234,
+    milestoneTitle: "로그인/로그아웃 구현 첫째날",
+    milestoneId: 12,
+    taskGroupTitle: "해야할일 - 1",
+    taskGroupId: 123,
+    created_at: "2023.11.20",
+    author_profile: "/profile.jpg",
+  },
+  {
+    title: "프로젝트에 인증/인가를 도입하며 배운점",
+    content: `# 프로젝트를 통해 배운 점 
+## 배고픔을 이겨내는 방법 
+## 배고픔은 항상 다가온다`,
+    author: "임준혁",
+    id: 1234,
+    projectTitle: "리액트 톺아보기",
+    projectId: 1234,
+    milestoneTitle: "로그인/로그아웃 구현 첫째날",
+    milestoneId: 12,
+    taskGroupTitle: "해야할일 - 1",
+    taskGroupId: 123,
+    created_at: "2023.11.20",
+    author_profile: "/profile.jpg",
+  },
+  {
+    title: "프로젝트에 인증/인가를 도입하며 배운점입니다. 어려웠어요",
+    content: `# 프로젝트를 통해 배운 점 
+## 배고픔을 이겨내는 방법 
+## 배고픔은 항상 다가온다`,
+    author: "임준혁",
+    id: 1235,
+    projectTitle: "리액트 톺아보기",
+    projectId: 1234,
+    milestoneTitle: "로그인/로그아웃 구현 첫째날",
+    milestoneId: 12,
+    taskGroupTitle: "해야할일 - 1",
+    taskGroupId: 123,
+    created_at: "2023.11.20",
+    author_profile: "/profile.jpg",
+  },
+  {
+    title: "프로젝트에 인증/인가를 도입하며 배운점입니다. 어려웠어요",
+    content: `# 프로젝트를 통해 배운 점 
+## 배고픔을 이겨내는 방법 
+## 배고픔은 항상 다가온다`,
+    author: "임준혁",
+    id: 1235,
+    projectTitle: "리액트 톺아보기",
+    projectId: 1234,
+    milestoneTitle: "로그인/로그아웃 구현 첫째날",
+    milestoneId: 12,
+    taskGroupTitle: "해야할일 - 1",
+    taskGroupId: 123,
+    created_at: "2023.11.20",
+    author_profile: "/profile.jpg",
+  },
+  {
+    title: "프로젝트에 인증/인가를 도입하며 배운점입니다. 어려웠어요",
+    content: `# 프로젝트를 통해 배운 점 
+## 배고픔을 이겨내는 방법 
+## 배고픔은 항상 다가온다`,
+    author: "임준혁",
+    id: 1235,
+    projectTitle: "리액트 톺아보기",
+    projectId: 1234,
+    milestoneTitle: "로그인/로그아웃 구현 첫째날",
+    milestoneId: 12,
+    taskGroupTitle: "해야할일 - 1",
+    taskGroupId: 123,
+    created_at: "2023.11.20",
+    author_profile: "/profile.jpg",
+  },
+];
