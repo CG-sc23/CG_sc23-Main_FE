182eff24c28c957e2cebe31ff5d253631838325a
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-11-19 17:48:07 +0900

    [Feat] 메인 페이지 구현중

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,57 @@
-// import Card from '@/components/Card';
-import client from '@/api/client';
-import useSignOut from '@/hooks/auth/useSignOut';
-import { queryKey } from '@/libs/constant';
-import { css } from '@emotion/react';
-import { safeLocalStorage } from '@toss/storage';
-import Link from 'next/link';
-// import { colors } from '@/components/constant/color';
-// import InputWithLabel from '@/components/InputWithLabel';
+import { queryKey } from "@/libs/constant";
+import styled from "@emotion/styled";
+import { safeLocalStorage } from "@toss/storage";
+import { bpmax } from "@/libs/styles/constants";
+import { Commerce } from "@/components/Feed/Commerce";
+import { UserCarousel } from "@/components/Feed/UserCarousel";
+import { Project } from "@/components/Feed/Project";
+
+// TODO : Project Recommend Carousel, Tasks
+
+// Container for main page
+const Container = styled.div`
+  width: 896px;
+  margin: 0 auto;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 24px;
+  ${bpmax[0]} {
+    width: 100%;
+    padding: 0;
+    gap: 1rem;
+  }
+`;
 
 export default function Home() {
-  const { signOut } = useSignOut();
   const token = safeLocalStorage.get(queryKey.USER_ACCESS_TOKEN);
 
   return (
-    <div
-      css={css`
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-      `}
-    >
-      <Link href="/auth/SignIn">SignIn</Link>
-      <Link href="/auth/SignUp">SignUp</Link>
-      <button
-        type="button"
-        onClick={() => {
-          console.log('SIGN OUT');
-          signOut();
-        }}
-      >
-        Sign Out
-      </button>
-      <button
-        type="button"
-        onClick={async () => {
-          console.log('UPDATE STATUS');
-          if (!token) return;
-          const res = await client.gitHubUpdateStatus({ token });
-          console.log(res);
-        }}
-      >
-        UPDATE STATUS
-      </button>
-      <button
-        type="button"
-        onClick={async () => {
-          console.log('STACK');
-          if (!token) return;
-          const res = await client.gitHubStack({ token });
-          console.log(res);
-        }}
-      >
-        STACK
-      </button>
-      <button
-        type="button"
-        onClick={async () => {
-          console.log('MANUAL_UPDATE');
-          if (!token) return;
-          const res = await client.gitHubManualUpdate({ token });
-          console.log(res);
-        }}
-      >
-        MANUAL_UPDATE
-      </button>
-    </div>
+    <Container>
+      <Project
+        title="나는 프론트엔드가 싫어요"
+        projectId="1234"
+        projectTitle="인증/인가"
+        author="임준혁"
+        likes={12}
+      />
+      <Project
+        title="나는 프론트엔드가 싫어요"
+        projectId="1234"
+        projectTitle="인증/인가"
+        author="임준혁"
+        likes={12}
+      />
+      <Project
+        title="나는 프론트엔드가 싫어요"
+        projectId="1234"
+        projectTitle="인증/인가"
+        author="임준혁"
+        likes={12}
+      />
+      <UserCarousel />
+      <Commerce imageUrl={undefined} />
+    </Container>
   );
 }
