2a1e6a272395c39141789c7a9e8a080bcd85c2da
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-11-20 01:47:47 +0900

    [Feat] 메인페이지에 들어갈 컴포넌트내의 제목 + children 구조 정의

--- src/components/Feed/Block.tsx
+++ src/components/Feed/Block.tsx
@@ -3,19 +3,53 @@ import { css } from "@emotion/react";
 import Link from "next/link";
 import Card from "../Card";
 import { FaChevronRight } from "react-icons/fa6";
+import { bpmax, bpmin } from "@/libs/styles/constants";
 
-type Props = { children : ReactNode, title : string, showChevron : boolean, href : string | null | undefined };
+type Props = {
+  children: ReactNode;
+  title?: string;
+  showChevron?: boolean;
+  hasTitle?: boolean;
+  href?: string;
+};
 
-export function Block({ children, title, showChevron = true, href} : Props) {
+export function Block({
+  children,
+  hasTitle = false,
+  title,
+  showChevron = true,
+  href,
+}: Props) {
   return (
     <Card>
-      <div css={css`display : flex; gap : 1rem;`}>
-        <h2 css={css`font-weight : 500;`}>{title}</h2>
-        <Link href={href ?? "/"}>
-          <FaChevronRight />
+      {hasTitle ? (
+        <Link
+          href={href ?? "/"}
+          css={css`
+            display: flex;
+            align-items: center;
+            gap: 0.5rem;
+
+            /* mobile */
+            ${bpmax[0]} {
+              margin-top: 1rem;
+            }
+          `}
+        >
+          <h2
+            css={css`
+              font-size: 1.2rem;
+              font-weight: 500;
+            `}
+          >
+            {title}
+          </h2>
+          {showChevron ? <FaChevronRight /> : <></>}
         </Link>
-      </div>
+      ) : (
+        <></>
+      )}
       {children}
     </Card>
-    );
+  );
 }
