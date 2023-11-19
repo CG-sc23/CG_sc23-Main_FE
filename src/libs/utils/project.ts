76772a62c1b5eafb4e27a1ec16a4d55f64cc91d4
limjunhyuk97 <limjunhyuk97@gmail.com>
2023-11-19 17:49:10 +0900

    [Feat] 프로젝트 진행사항 표시 위한 util함수

---
+++ src/libs/utils/project.ts
@@ -0,0 +1,28 @@
+import { colors } from "@/components/constant/color";
+
+type ProjectStatus = {
+  text: string;
+  color: string;
+};
+
+export function myProjectStatus(provider: string): ProjectStatus {
+  const strategies = {
+    in_progress: {
+      text: "진행중",
+      color: colors.green500,
+    },
+    complete: {
+      text: "프로젝트 종료",
+      color: colors.red500,
+    },
+    terminated: {
+      text: "프로젝트 포기",
+      color: colors.grey500,
+    },
+    default: {
+      text: "오류",
+      color: colors.grey500,
+    },
+  } as const;
+  return strategies[provider as keyof typeof strategies] ?? strategies.default;
+}
