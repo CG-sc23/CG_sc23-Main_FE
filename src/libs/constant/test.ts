b94cb00299e2e9bddb5077990db360ddc6c419ab
jun_lim97 <limjunhyuk97@gmail.com>
2023-11-19 23:32:22 +0900

    [ETC] 테스트 위한 mock 데이터 임시 저장

---
+++ src/libs/constant/test.ts
@@ -0,0 +1,94 @@
+// Test Data for User Carousel in Feed
+export const UserDatas = [
+  {
+    name: "임준혁",
+    profile: "/profile.jpg",
+    id: "12",
+    short_description: "안녕하세요, 임준혁입니다. 환영합니다. DOMO입니다.",
+  },
+  {
+    name: "임준혁",
+    profile: "/profile.jpg",
+    id: "12",
+    short_description: "안녕하세요, 임준혁입니다. 환영합니다. DOMO입니다.",
+  },
+  {
+    name: "임준혁",
+    profile: "/profile.jpg",
+    id: "12",
+    short_description: "안녕하세요, 임준혁입니다. 환영합니다. DOMO입니다.",
+  },
+  {
+    name: "임준혁",
+    profile: "/profile.jpg",
+    id: "12",
+    short_description: "안녕하세요, 임준혁입니다. 환영합니다. DOMO입니다.",
+  },
+  {
+    name: "임준혁",
+    profile: "/profile.jpg",
+    id: "12",
+    short_description: "안녕하세요, 임준혁입니다. 환영합니다. DOMO입니다.",
+  },
+  {
+    name: "임준혁",
+    profile: "/profile.jpg",
+    id: "12",
+    short_description: "안녕하세요, 임준혁입니다. 환영합니다. DOMO입니다.",
+  },
+  {
+    name: "임준혁",
+    profile: "/profile.jpg",
+    id: "12",
+    short_description: "안녕하세요, 임준혁입니다. 환영합니다. DOMO입니다.",
+  },
+];
+
+// Test Data for Project in Feed
+export const ProjectDatas = [
+  {
+    title :"나는 프론트엔드가 싫어요",
+    projectId :1234,
+    projectTitle :"인증/인가",
+    owner:"임준혁",
+    likes:12,
+    id:12,
+    created_at : '2023.11.19'
+  },
+  {
+    title :"나는 프론트엔드가 싫어요",
+    projectId :1234,
+    projectTitle :"인증/인가",
+    owner:"임준혁",
+    likes:12,
+    id:12,
+    created_at : '2023.11.19'
+  },
+  {
+    title :"나는 프론트엔드가 싫어요",
+    projectId :1234,
+    projectTitle :"인증/인가",
+    owner:"임준혁",
+    likes:12,
+    id:12,
+    created_at : '2023.11.19'
+  },
+  {
+    title :"나는 프론트엔드가 싫어요",
+    projectId :1234,
+    projectTitle :"인증/인가",
+    owner:"임준혁",
+    likes:12,
+    id:12,
+    created_at : '2023.11.19'
+  },
+  {
+    title :"나는 프론트엔드가 싫어요",
+    projectId :1234,
+    projectTitle :"인증/인가",
+    owner:"임준혁",
+    likes:12,
+    id:12,
+    created_at : '2023.11.19'
+  }
+]
