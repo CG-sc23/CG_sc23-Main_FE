b7077889c0d834a1128e41fbf77b9127769b011a
LUH <unghee66@gmail.com>
2023-11-27 19:09:23 +0900

    [CI/CD] Fix path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'DOMO',
-      script: '/run/user/1000/fnm_multishells/9564_1701077243664/bin/yarn',
+      script: 'yarn',
       args: 'run start',
       autorestart: true,
       watch: false,
